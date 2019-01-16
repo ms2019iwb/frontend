390609dd9aee6855e5365538824cae94f67626a2
hirotowatanabe <hiroto.private@gmail.com>
2019-01-17 06:35:23 +0900

    最新の投稿バックエンド連携・デザイン修正

--- src/containers/Top.js
+++ src/containers/Top.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import Header from '../components/Header';
 import axios from 'axios';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFire, faFireExtinguisher } from '@fortawesome/free-solid-svg-icons';
 import Variable from '../variables/Variable';
 
@@ -13,58 +14,101 @@ class Top extends Component {
       // 炎上中総数
       burnNumber: 0,
       // 消火済み総数
-      exNumber: 0
+      exNumber: 0,
+      // 最新のレポート
+      latestReport: {
+        id: 0,
+        title: '',
+        fire_fighting_status: '',
+        created_at: '',
+        updated_at: ''
+      }
     }
   }
 
   async componentDidMount() {
     await this.fetchBurnCount();
     await this.fetchExCount();
+    await this.fetchLatestReport();
   }
 
   // 炎上中総数取得
   fetchBurnCount() {
     return new Promise((resolve, reject) => {
-      console.log('【frego-api】HTTPリクエスト開始: GET /posts/burncount');
+      console.log('【frego-api】HTTPリクエスト開始: GET /burncount');
 
       axios
-      .get(`${Variable.FREGO_API_BASE_ENDPOINT}/burncount`)
-      .then(response => {
-        if(response.data) {
-          console.log('【frego-api】HTTPリクエスト正常終了: ', response.data);
-          this.setState({
-            burnNumber: response.data
-          });
-          resolve();
-        }
-      })
-      .catch(error => {
-        console.log('【frego-api】HTTPリクエスト異常終了', error);
-        reject();
+        .get(`${Variable.FREGO_API_BASE_ENDPOINT}/burncount`)
+        .then(response => {
+          if(response.data) {
+            console.log('【frego-api】HTTPリクエスト正常終了: ', response.data);
+            this.setState({
+              burnNumber: response.data
+            });
+            resolve();
+          }
+        })
+        .catch(error => {
+          console.log('【frego-api】HTTPリクエスト異常終了', error);
+          reject();
+        });
       });
-    });
   }
 
   // 消火済み総数取得
   fetchExCount() {
     return new Promise((resolve, reject) => {
-      console.log('【frego-api】HTTPリクエスト開始: GET /posts/excount');
+      console.log('【frego-api】HTTPリクエスト開始: GET /excount');
 
       axios
-      .get(`${Variable.FREGO_API_BASE_ENDPOINT}/excount`)
-      .then(response => {
-        if(response.data) {
-          console.log('【frego-api】HTTPリクエスト正常終了: ', response.data);
-          this.setState({
-            exNumber: response.data
-          });
-          resolve();
-        }
-      })
-      .catch(error => {
-        console.log('【frego-api】HTTPリクエスト異常終了', error);
-        reject();
+        .get(`${Variable.FREGO_API_BASE_ENDPOINT}/excount`)
+        .then(response => {
+          if(response.data) {
+            console.log('【frego-api】HTTPリクエスト正常終了: ', response.data);
+            this.setState({
+              exNumber: response.data
+            });
+            resolve();
+          }
+        })
+        .catch(error => {
+          console.log('【frego-api】HTTPリクエスト異常終了', error);
+          reject();
+        });
       });
+  }
+
+  // 最新の投稿取得
+  fetchLatestReport() {
+    return new Promise((resolve, reject) => {
+      console.log('【frego-api】HTTPリクエスト開始: GET /lastpost');
+
+      axios
+        .get(`${Variable.FREGO_API_BASE_ENDPOINT}/lastpost`)
+        .then(response => {
+          if(response.data) {
+            console.log('【frego-api】HTTPリクエスト正常終了: ', response.data);
+            // 日付整形
+            const cdt = new Date(response.data.created_at);
+            const preformattedCreatedAt = `${cdt.getFullYear()}/${cdt.getMonth()+1}/${cdt.getDate()} ${cdt.getHours()}:${cdt.getMinutes()}`;
+            const udt = new Date(response.data.updated_at);
+            const preformattedUpdatedAt = `${udt.getFullYear()}/${udt.getMonth()+1}/${udt.getDate()} ${udt.getHours()}:${udt.getMinutes()}`;
+            this.setState({
+              latestReport: {
+                id: response.data.id,
+                title: response.data.title,
+                fire_fighting_status: response.data.fire_fighting_status,
+                created_at: preformattedCreatedAt,
+                updated_at: preformattedUpdatedAt
+              }
+            });
+            resolve();
+          }
+        })
+        .catch(error => {
+          console.log('【frego-api】HTTPリクエスト異常終了', error);
+          reject();
+        });
     });
   }
 
@@ -84,25 +128,36 @@ class Top extends Component {
           </Extinguishingcount>
         </Countzone>
         <Toppost>
-          <Totitle>
-            最新の投稿
-          </Totitle>
+          <SectionTitle>最新の投稿</SectionTitle>
           <Tocontents>
             <Totext>
-              <Place>発生場所：XXX県XXX市XXX町XX-X</Place>
-              <Potime>発生日時：XXXX年XX月XX日</Potime>
-              <Uptime>更新日時：XXXX年XX月XX日</Uptime>
+              <Place>
+                <LatestReportLink to={`/report/${this.state.latestReport.id}`}>{this.state.latestReport.title}</LatestReportLink>
+              </Place>
+              <Potime>発生日時：{this.state.latestReport.created_at}</Potime>
+              <Uptime>更新日時：{this.state.latestReport.updated_at}</Uptime>
             </Totext>
-            <Toimage><FontAwesomeIcon icon={faFire} /></Toimage>
+            <Toimage><StatusIcon iconColor={() => {
+              switch(this.state.latestReport.fire_fighting_status) {
+                // 未消火
+                case '1':
+                  return '#ff000f';
+                  break;
+                // 消火済み
+                case '2':
+                  return '#254fae';
+                  break;
+                // 不明
+                case '3':
+                  return '#777';
+                  break;
+              }
+            }} icon={faFire} /></Toimage>
           </Tocontents>
-          <Postlist>
-            投稿一覧
-          </Postlist>
+          <ListLink to="/report">投稿一覧</ListLink>
         </Toppost>
         <Newupdate>
-          <Netitle>
-            最近の更新
-          </Netitle>
+          <SectionTitle>最近の更新</SectionTitle>
           <Necontents>
             <Netext>XXX県XXX市XXX町XX-X の火災レポートの概要が更新されました。</Netext>
             <Netext>YYY県YYY市YYY町YY-Y の火災レポートに新規のコメントがあります。</Netext>
@@ -110,7 +165,7 @@ class Top extends Component {
             <Netext>AAA県AAA市AAA町AA-A の火災レポートに新規のコメントがあります。</Netext>
             <Netext>XXX県XXX市XXX町XX-X の火災レポートの概要が更新されました。</Netext>
           </Necontents>
-          <Uplist>更新一覧</Uplist>
+          <ListLink to="">更新一覧</ListLink>
         </Newupdate>
       </Root>
     );
@@ -123,7 +178,7 @@ const Root = styled.div``
 
 const Title = styled.h1`
   margin: 32px 0 30px 0;
-  color: #A552DD;
+  color: #ff000f;
   font-size: 2.6rem;
   text-align: center;
 `
@@ -141,7 +196,7 @@ const Firecount = styled.div`
   width: 350px;
   margin-right: 102px;
   box-shadow: 0px 0px 10px rgba(0,0,0,0.16);
-  background-color: #ec1d1d;
+  background-color: #F84F59;
 `
 
 const Finumber = styled.div`
@@ -174,7 +229,7 @@ const Extinguishingcount = styled.div`
   width: 350px;
   border: 1px solid #707070;
   box-shadow: 0px 0px 10px rgba(0,0,0,0.16);
-  background-color: #254FAE;
+  background-color: #554FF8;
 `
 
 const Exnumber = styled.div`
@@ -204,10 +259,10 @@ const Toppost = styled.div`
   margin-bottom: 22px;
 `
 
-const Totitle = styled.div`
-  color: #A552DD;
-  font-size: 2.4rem;
+const SectionTitle = styled.div`
   line-height: 36px;
+  font-size: 2.4rem;
+  color: #FF000F;
 `
 
 const Tocontents = styled.div`
@@ -226,9 +281,13 @@ const Totext = styled.div`
 const Place = styled.div`
   padding-left: 11px;
   border-bottom: 1px solid #707070;
-  color: #3F2B2B;
-  font-size: 2rem;
   line-height: 35px;
+`
+
+const LatestReportLink = styled(Link)`
+  font-size: 2rem;
+  text-decoration: underline;
+  color: #3f2b2b;
 `
 
 const Potime = styled.div`
@@ -248,21 +307,26 @@ const Uptime = styled.div`
 const Toimage = styled.div`
   width: 100px;
   text-align: center;
-  & > svg{
-    width: 58px !important;
-    height: auto !important;
-    color: #ec1d1d;
-  }
 `
 
-const Postlist = styled.div`
+const StatusIcon = styled(FontAwesomeIcon)`
+  font-size: 5.8rem;
+  color: ${props => props.iconColor};
+`
+
+const ListLink = styled(Link)`
+  display: block;
+  border-radius: 5px;
+  background-color: #FF000F;
   height: 40px;
-  box-shadow: 0px 3px 6px rgba(0,0,0,0.16);
-  background-color: #8639ba;
-  color: #FFFFFF;
-  font-size: 2.4rem;
   line-height: 40px;
   text-align: center;
+  font-size: 2.4rem;
+  color: #FFFFFF;
+
+  &:hover {
+    box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.6);
+  }
 `
 
 const Newupdate = styled.div`
@@ -292,14 +356,4 @@ const Netext = styled.div`
   &:not(:last-child) {
     border-bottom: 1px solid #707070;
   }
-`
-
-const Uplist = styled.div`
-  height: 40px;
-  box-shadow: 0px 3px 6px rgba(0,0,0,0.16);
-  background-color: #8639ba;
-  color: #FFFFFF;
-  font-size: 2.4rem;
-  line-height: 40px;
-  text-align: center;
 `
